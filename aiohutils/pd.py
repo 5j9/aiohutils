0f039c7120ae7cbddd0aa44a2ca5d66623be4898
5j9 <5j9@users.noreply.github.com>
2024-03-24 20:28:44 +0330

    feat(pd.from_html): new func

--- aiohutils/pd.py
+++ aiohutils/pd.py
@@ -1,7 +1,18 @@
 from io import StringIO
+from warnings import warn
 
-from pandas import DataFrame, read_html
+from pandas import read_html
+from polars import DataFrame, from_pandas
 
 
 def html_to_df(html: str, index=0, /, **kwargs) -> DataFrame:
+    warn('use from_html instead', DeprecationWarning)
     return read_html(StringIO(html), **kwargs)[index]
+
+
+def from_html(html: str, index=0, /, **kwargs) -> DataFrame | list[DataFrame]:
+    # todo: use lxml directly to avoid pandas
+    rh = read_html(StringIO(html, **kwargs))
+    if index is None:
+        return [from_pandas(df) for df in rh]
+    return from_pandas(rh[index])
