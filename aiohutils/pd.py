058be8cc81091d9ef5c78d0c8a7d2f3f1edef934
5j9 <5j9@users.noreply.github.com>
2024-03-26 17:29:43 +0330

    fix(pd.html_to_df)!: return pd.DataFrame not pl.DataFrame
    move from_html to sep module.

--- aiohutils/pd.py
+++ aiohutils/pd.py
@@ -1,16 +1,7 @@
 from io import StringIO
 
-from pandas import read_html
-from polars import DataFrame, from_pandas
+from pandas import DataFrame, read_html
 
 
 def html_to_df(html: str, index=0, /, **kwargs) -> DataFrame:
     return read_html(StringIO(html), **kwargs)[index]
-
-
-def from_html(html: str, index=0, /, **kwargs) -> DataFrame | list[DataFrame]:
-    # todo: use lxml directly to avoid pandas
-    rh = read_html(StringIO(html, **kwargs))
-    if index is None:
-        return [from_pandas(df) for df in rh]
-    return from_pandas(rh[index])
