3bdb6b15419b62efc0c210a80284f894bf77e045
5j9 <5j9@users.noreply.github.com>
2024-10-30 16:44:01 +0330

    feat(server.serve_static): new method
    deprecate static_path

--- aiohutils/server.py
+++ aiohutils/server.py
@@ -1,12 +1,16 @@
 from pathlib import Path
+from warnings import warn
 from zlib import adler32
 
-from aiohttp.web_fileresponse import FileResponse
-from aiohttp.web_request import Request
-from aiohttp.web_routedef import RouteDef, get
+from aiohttp.web import Application, FileResponse, Request, RouteDef, get
 
 
 def static_path(file: Path) -> tuple[str, RouteDef]:
+    warn(
+        'aiohutils.static_path is deprecated; use aiohutils.serve_static instead.',
+        DeprecationWarning,
+        stacklevel=2,
+    )
     serve_path = (
         f'/static/{adler32(str(file.absolute()).encode())}/{file.name}'
     )
@@ -17,3 +21,12 @@ def static_path(file: Path) -> tuple[str, RouteDef]:
         )
 
     return serve_path, get(serve_path, handler)
+
+
+def serve_static(app: Application, file: Path) -> None:
+    async def handler(_: Request) -> FileResponse:
+        return FileResponse(
+            file, headers={'Cache-Control': 'immutable,   max-age=604800'}
+        )
+
+    app.router.add_get(f'/static/{file.name}', handler)
